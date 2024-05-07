1ccec8d2caa040e4b9afa8dde3cd57fb7c06c37c
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-05-07 09:52:36 +0200

    edit task 3 p3

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(97,123):
-    if i != 101 and i != 113 :
-     print(chr(i))
+for i in range(ord('a'), ord('z') + 1):
+    if i != ord('e') and i != ord('q'):
+        print("{}".format(chr(i)), end="")
