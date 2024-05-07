598b414931a81e9d90c2a9c15a89d6464c46ccc9
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-05-07 11:22:09 +0200

    edit the task 5 p3

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,4 @@
-#!/user/bin/python3
-for i in range(0, 100):
-    if i != 99:
-        print("{:02d}, ".format(i), end="")
-    else:
-        print("{:02d}".format(i))
+#!/usr/bin/python3
+for number in range(0, 99):
+    print("{:02}".format(number), end=", ")
+print("99")
