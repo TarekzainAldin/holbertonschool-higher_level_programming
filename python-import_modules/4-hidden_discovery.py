1b803d6f62c4e451ecbfc13849e0c1c1eb884028
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-05-08 15:10:11 +0200

    try another code

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    """print all hidden directores"""
+if __name__ == '__main__':
     import hidden_4
-    for i in dir(hidden_4):
-        if i[:2] != ("__"):
-            print(i)
+    names_list = dir(hidden_4)
+    for name in names_list:
+        if name[0] != '__':
+            print("{}".format(name))
