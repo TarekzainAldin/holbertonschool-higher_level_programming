033d16ebaaa11ec6bb759a9570ee1c4d7ae15681
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-05-14 14:32:59 +0200

    edit the code

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,3 +1,3 @@
-#!user/bin/python3
-def common_elments(set_1, set_2):
-    return {ele for ele in set_1 if ele in set_2}
+#!/usr/bin/python3
+def common_elements(set_1, set_2):
+    return {element for element in set_1 if element in set_2}
