fb848444137354b49c2ba14b4a26faff38de27fb
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-05-14 15:45:28 +0200

    edit the code

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        for keyes, valeur in a_dictionary.items():
-            if valeur == max(a_dictionary.values()):
+        for keyes, val in a_dictionary.items():
+            if val == max(a_dictionary.values()):
                 return keyes
