0e45837e7fb39c0df7671c622f1c258c252d1b18
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-05-15 14:47:35 +0200

    edit the code

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
-#!/user/bin/python3
-def sqfe_print_list(my_list=[], x=0):
-    number = 0
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    count = 0
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
-            number += 1
+            count += 1
         except IndexError:
             break
-        print()
-        return number
+    print()
+    return count
