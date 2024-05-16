bbc2eb98405e221747ab85a0837b234fb18faa02
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-05-16 18:09:42 +0200

    zs

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,55 +1,49 @@
-#!/usr/bin/python3
-"""create an empty class called Square"""
-
-
 class Square:
-    """create a square.
-    size (no type/value verification)
-    atributes:
-    size: private instance.
-    """
+    """Represents a square."""
 
     def __init__(self, size=0, position=(0, 0)):
+        """Initializes the Square instance."""
         self.size = size
         self.position = position
 
-    def area(self):
-        return self.__size * self.__size
-
     @property
     def size(self):
+        """Getter method for size."""
         return self.__size
 
     @size.setter
     def size(self, value):
-        if type(value) != int:
+        """Setter method for size."""
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
+        self.__size = value
 
     @property
     def position(self):
+        """Getter method for position."""
         return self.__position
 
     @position.setter
     def position(self, value):
-        if (type(value) != tuple or len(value) != 2 or
-                value[0] < 0 or type(value[0]) != int or
-                type(value[1]) != int or value[1] < 0):
+        """Setter method for position."""
+        if not isinstance(value, tuple) or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not all(isinstance(num, int) for num in value) or value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
+    def area(self):
+        """Calculates the area of the square."""
+        return self.__size ** 2
+
     def my_print(self):
+        """Prints the square with '#' characters."""
         if self.__size == 0:
             print()
-            return
-        for y in range(self.__position[1]):
-            print()
-        for h in range(self.__size):
-            for p in range(self.__position[0]):
-                print(" ", end="")
-            for w in range(self.__size - 1):
-                print('#', end="")
-            print('#')
+        else:
+            for _ in range(self.__position[1]):
+                print()
+            for _ in range(self.__size):
+                print(" " * self.__position[0] + "#" * self.__size)
