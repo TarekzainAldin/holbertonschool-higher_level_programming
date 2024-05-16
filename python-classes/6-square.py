02835da0d19b0930eaeb78b98674be4b75e5a659
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-05-16 18:15:06 +0200

    ed

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,53 +1,53 @@
 #!/usr/bin/python3
-"""Contains an class : 'Square'"""
+"""Module contenant la classe Square."""
 
 
 class Square:
-    """A class representing a square."""
+    """Classe représentant un carré."""
 
     def __init__(self, size=0, position=(0, 0)):
-        """Initialize the Square with a given size."""
+        """Initialise un carré avec une taille optionnelle."""
         self.size = size
         self.position = position
 
     @property
     def size(self):
-        """Get the size of the square."""
-        return self.__size
-
-    @property
-    def position(self):
-        """Get the position of the square."""
-        return self.__position
+        """Obtient la taille du carré."""
+        return self._size
 
     @size.setter
     def size(self, value):
-        """Set the size of the square."""
-        if not type(value) is int:
-            raise Exception("size must be an integer")
+        """Définit la taille du carré avec des vérifications."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
         if value < 0:
-            raise Exception("size must be >= 0")
-        self.__size = value
+            raise ValueError("size must be >= 0")
+        self._size = value
+
+    @property
+    def position(self):
+        """obtient la position du carre"""
+        return self._position
 
     @position.setter
     def position(self, value):
-        """Set the position of the square."""
-        cond1 = type(value) is tuple and len(value) == 2
-        cond2 = type(value[0]) is int and type(value[1]) is int
-        cond3 = value[0] >= 0 and value[1] >= 0
-        if cond1 and cond2 and cond3:
-            self.__position = value
-        else:
-            raise Exception("position must be a tuple of 2 positive integers")
+        """Définit la position du carré avec des vérifications."""
+        if (not isinstance(value, tuple) or len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self._position = value
 
     def area(self):
-        """Return square of size"""
-        return self.__size ** 2
+        """Calcule et retourne l'aire du carré."""
+        return (self._size ** 2)
 
     def my_print(self):
-        if self.size == 0:
-            print()
-        else:
-            print("\n" * self.position[1], end='')
-            for _ in range(self.size):
-                print(" " * self.position[0], "#" * self.size, sep='')
+        """imprimer la valeur de la taille"""
+        if self._size == 0:
+            print("")
+            return
+        for _ in range(self._position[1]):
+            print("")
+        for _ in range(self._size):
+            print(" " * self._position[0] + "#" * self._size)
