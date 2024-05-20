142e36d3522055e76edf17693012b7107297e7eb
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-05-20 14:45:45 +0200

    edit the code

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,22 +1,25 @@
-#!/user/bin/python3
-"""Moudle for say_my_name method"""
+#!/usr/bin/python3
+"""Module for say_my_name method."""
 
 
-def say_my_name(first_name, last_name):
-    """method for printing first and last name
-     args:
-     first_name string
-     last_name string
-     Raise:
-     typeError: if first_name or last_name not strings
-        """
+def say_my_name(first_name, last_name=""):
+    """Method for printing first and last name.
 
+    Args:
+        first_name: first name string.
+        last_name: last name string.
+
+    Raises:
+        TypeError: If first_name or last_name are not strings.
+    """
     if not isinstance(first_name, str):
-        raise TypeError("first_name must be string")
-    if not isinstance(last_name, str):
-        raise TypeError("last_name must be string")
-    print("My Name Is  {:s} {:s}".format(first_name, last_name))
+        raise TypeError("first_name must be a string")
 
-    if __name__ == "__main__":
-        import doctest
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print("My name is {:s} {:s}".format(first_name, last_name))
+
+if __name__ == "__main__":
+    import doctest
     doctest.testfile("tests/3-say_my_name.txt")
