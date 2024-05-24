4b1103109cc0381144b5833f65ccf1bfa8407bb9
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-05-24 13:49:41 +0200

    make the basic files and the first task abc class and inhertes

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,25 @@
+#!/user/bin/pytho3
+"""abstract class"""
+
+from abc import ABS, abstractmethod
+
+
+# abstract mainclass
+class Animal(ABS):
+
+    @abstractmethod
+    def sound(self):
+        pass
+
+
+# subclass inherts form the animal
+class Dog(Animal):
+
+    def sound(self):
+        return "Bark"
+
+
+# subclass inherts from the animal
+class cat(Animal):
+    def sound(self):
+        return "Meow"
