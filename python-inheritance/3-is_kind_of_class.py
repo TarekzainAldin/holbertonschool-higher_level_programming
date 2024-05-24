ffbd9aa8303eb741f236fba612b1599e735d0934
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-05-24 10:38:41 +0200

    edit the code

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,9 +1,9 @@
-#!/user/bin/python3
-"""is_kiind_of_class"""
+#!/usr/bin/python3
+"""is_kind_of_class"""
 
 
 def is_kind_of_class(obj, a_class):
+    """if the object is an instance of a class"""
     if isinstance(obj, a_class):
         return True
-    else:
-        return False
+    return False
