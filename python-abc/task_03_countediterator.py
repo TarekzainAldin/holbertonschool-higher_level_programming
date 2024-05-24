16b9945f2e35c68687ccc49d715c0f12e667032c
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-05-24 15:48:39 +0200

    make the conteditraor

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+
+class CountedIterator:
+    def __init__(self, iterable):
+        self.iterator = iter(iterable)
+        self.counter = 0
+
+    def __next__(self):
+        try:
+            item = next(self.iterator)
+            self.counter += 1
+            return item
+        except StopIteration:
+            raise StopIteration("No more items to iterate.")
+
+    def get_count(self):
+        return self.counter
