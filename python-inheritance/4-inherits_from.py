ca88e35d20d2eb6ce049d56b3a24a2a3ea46bcb5
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-05-24 10:45:02 +0200

    edit the code

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,9 +1,9 @@
-#!/user/bin/python3
-"""ingerits"""
+#!/usr/bin/python3
+"""inherits_from"""
 
 
-def inherits_form(obj, a_class):
-    """inherites"""
+def inherits_from(obj, a_class):
+    """Write a function that returns True"""
     if isinstance(obj, a_class) and type(obj) is not a_class:
         return True
     return False
