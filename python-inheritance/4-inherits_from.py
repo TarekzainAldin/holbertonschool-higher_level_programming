168f57b60147f029780cae57657c33018d71d6f4
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-05-24 09:00:52 +0200

    chek if the instance is class

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/user/bin/python3
+"""ingerits"""
+
+
+def inherits_form(obj, a_class):
+    """inherites"""
+    if isinstance(obj, a_class) and type(obj) is not a_class:
+        return True
+    else:
+        return False
