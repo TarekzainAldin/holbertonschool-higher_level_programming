ba4977607f6471e8d417d889821709d4a8104255
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-05-27 18:32:41 +0200

    edit the code

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-"""class student"""
+"""this file contain the class student"""
 
 
 class Student:
+    """class of student"""
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-        def to_json(self):
-            """return  dictinaary"""
-            return self.__dict__
+    def to_json(self):
+        """return dictionary description of object"""
+        return self.__dict__
