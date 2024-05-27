8a9993a82dc320413a2f51f95b0cb0c91345f702
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-05-27 18:20:05 +0200

    edit the code

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,18 +1,9 @@
 #!/usr/bin/python3
-"""contains the Class to JSON"""
+"""contient la fonction class_to_json """
 
 
 def class_to_json(obj):
-    """class to json
+    """return  dictionnaire contenant les attributs de l'objet obj
+    'attribut' : valeur
     """
-    if not hasattr(obj, '__dict__'):
-        raise ValueError("Input object is not serializable")
-
-    serialized = {}
-    for key, value in obj.__dict__.items():
-        if isinstance(value, (list, dict, str, int, bool)):
-            serialized[key] = value
-        else:
-            raise ValueError(f"Attribute '{key}' is not serializable")
-
-    return serialized
+    return obj.__dict__
