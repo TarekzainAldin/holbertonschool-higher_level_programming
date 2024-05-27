232ff89c5699b997e198e985923f7ebafbeb7b32
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-05-27 18:41:15 +0200

    zd

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-"""contais the Create object from a JSON file"""
+"""Contains the Save Object to a file"""
+
 
 import json
 
 
-def load_from_json_file(filename):
-    """Creates an object from a JSON file."""
-    with open(filename, "r", encoding='utf-8') as f:
-        return json.load(f)
+def save_to_json_file(my_obj, filename):
+    """ function that writes an Object to a text file,
+    using a JSON representation"""
+    with open(filename, "w") as file:
+        json.dump(my_obj, file)
