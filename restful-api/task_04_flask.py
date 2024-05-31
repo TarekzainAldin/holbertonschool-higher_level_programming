422049d7f915114cec91f4423d9ca670e9ac3b91
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-05-31 13:16:40 +0200

    edit the code in the tase 4 and 5

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,37 +1,28 @@
 #!/usr/bin/python3
-"""the script for adding in the flask"""
-
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
-"""herer alsso decument"""
-users = {
-    "jane": {"name": "Jane", "age": 28, "city": "Los Angeles"},
-    "john": {"name": "John", "age": 30, "city": "New York"}
-}
+
+users = {}
 
 
-@app.route('/')
+@app.route("/")
 def home():
-    """the get of home"""
     return "Welcome to the Flask API!"
 
 
-@app.route('/data')
+@app.route("/data")
 def get_data():
-    """get the data in api"""
     return jsonify(list(users.keys()))
 
 
-@app.route('/status')
+@app.route("/status")
 def status():
-    """heere we are the stat for the users"""
     return "OK"
 
 
-@app.route('/users/<username>')
+@app.route("/users/<username>")
 def get_user(username):
-    """get user in the users"""
     user = users.get(username)
     if user:
         return jsonify(user)
@@ -39,22 +30,15 @@ def get_user(username):
         return jsonify({"error": "User not found"}), 404
 
 
-@app.route('/add_user', methods=['POST'])
+@app.route("/add_user", methods=["POST"])
 def add_user():
-    """adding user in the list of users by curl"""
-    user_data = request.json
-    username = user_data.get('username')
-    if username and username not in users:
-        users[username] = {
-            "name": user_data.get('name'),
-            "age": user_data.get('age'),
-            "city": user_data.get('city')
-        }
-        return jsonify({"message": "User added", "user": users[username]})
-    else:
-        return jsonify({"error": "Invalid data or user already exists"}), 400
+    user_data = request.get_json()
+    username = user_data.get("username")
+    if not username:
+        return jsonify({"error": "Username is required"}), 400
+    users[username] = user_data
+    return jsonify({"message": "User added", "user": user_data}), 201
 
 
-if __name__ == '__main__':
-    """when he is execute in directly"""
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run()
