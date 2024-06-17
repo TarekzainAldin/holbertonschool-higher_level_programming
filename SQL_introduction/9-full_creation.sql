a81b17783574d3d22b18a2ced5cb398ba570499a
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-06-17 16:47:15 +0200

    maket the scend table and adding 4 receords insaid

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,7 @@
+-- full creation 
+CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256),score INT);
+-- adding the vlues in the rows in the table after the creat database
+INSERT INTO second_table VALUES (1, 'John', 10);
+INSERT INTO second_table VALUES (2, 'Alex', 3);
+INSERT INTO second_table VALUES (3, 'Bob', 14);
+INSERT INTO second_table VALUES (4, 'George', 8);
