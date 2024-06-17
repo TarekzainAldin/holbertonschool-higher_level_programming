4811c5206d4020820327951d6dd8d0aed2382d11
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-06-17 17:17:47 +0200

    select the > 10 and update the table set scor 10

---
+++ SQL_introduction/11-best_score.sql
@@ -0,0 +1,2 @@
+-- select  the score > 10 and secd by score 
+SELECT name, score FROM second_table where score >= 10 ORDER BY score DESC;
---
+++ SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,2 @@
+-- update the value for Bob  
+UPDATE second_table set score =10 where name = 'BOb';
