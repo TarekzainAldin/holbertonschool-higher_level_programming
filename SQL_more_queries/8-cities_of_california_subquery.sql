2c35b732705e3a5bad82843217b6fe6876115b63
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-06-18 17:30:36 +0200

    edit the code

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,4 @@
-SET @california_id := (SELECT id FROM states WHERE name = 'California');
 SELECT id, name
 FROM cities
-WHERE state_id = @california_id
+WHERE state_id = (SELECT id FROM states WHERE name="California")
 ORDER BY id ASC;
