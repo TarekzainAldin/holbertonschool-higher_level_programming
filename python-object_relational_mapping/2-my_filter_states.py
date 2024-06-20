32ad098dad48b62ec712f1c478c60a76b3be55aa
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-06-20 11:17:52 +0200

    the filter in the argement value and make the chmode

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Write a script that takes in an argument and
+displays all values in the states table of
+hbtn_0e_0_usa where name matches the argument."""
+import MYSQLdb
+import sys
+
+
+if __name__ == '__main__':
+    db = MYSQLdb.connect(
+        host='localhost',
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306
+    )
+    cur = cursor()
+    cur.execute("""SELECT * FROM states WHERE name \
+                 = % s ORDER BY states.id ASC """ (sys.argv[4],))
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+        cur.close()
+        db.close()
