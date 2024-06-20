770244080d0c629ece27b31c750d8b7223e87f99
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-06-20 11:26:57 +0200

    edit the code

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,30 +1,28 @@
 #!/usr/bin/python3
-"""Write a script that takes in an argument and
-displays all values in the states table of
-hbtn_0e_0_usa where name matches the argument."""
-import MySQLdb
-import sys
+"""Module listing all states with a name that matc the arg from the database"""
 
+import MySQLdb
+from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(
-        host='localhost',
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        port=3306
-    )
+    # Connect to the database
+    db = MySQLdb.connect("localhost", argv[1], argv[2], argv[3])
 
+    # Create a cursor object to interact with the database
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name \
-                    LIKE BINARY '{}' ORDER BY states.id ASC"
-                   .format(sys.argv[4])
-                   )
 
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
+    # Execute the SQL query with parameterized input
+    cur.execute(
+        """SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"""
+        .format(argv[4]))
+    # Fetch all the rows returned by the query
+    query_rows = cur.fetchall()
 
+    # Print  row
+    for row in query_rows:
+        if row[1] == argv[4]:
+            print(row)
+
+    # Close the cursor and connection
     cur.close()
     db.close()
-    
