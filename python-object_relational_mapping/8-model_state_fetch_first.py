682d38c5249534e203330a3533e429f9d9a5b363
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-06-21 10:25:16 +0200

    edit the task

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -3,26 +3,38 @@
 the first State object from the database hbtn_0e_6_us """
 
 
-import MySQLdb
 import sys
-from model_state import Base, State
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
-    Base.metadata.create_all(engine)
+    if len(sys.argv) != 4:
+        print("usage: {} username password database".format(sys.argv[0]))
+        sys.exit(1)
 
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Database connection
+    engine = create_engine(
+                           'mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(username, password, database))
+
+    # Create a configured "Session" class
     Session = sessionmaker(bind=engine)
+
+    # Create a Session
     session = Session()
 
-    state = session.query(State).first()
+    # Query for the first State object ordered by id
+    first_state = session.query(State).order_by(State.id).first()
 
-    if state:
-        print(f'{state.id}: {state.name}')
+    if first_state:
+        print("{}".format(first_state.id) + ": " + "{}".
+              format(first_state.name))
     else:
-        print('Nothing')
-session.commit()
-session.close()
+        print("Nothing")
+
+    session.close()
