9a07e610c77b09d928b1dd39ceef1ab54037e7b0
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-07-15 19:49:52 +0200

    make the tamplates folder and the files index about conect header and fotter and file jinja.py

---
+++ python-server_side_rendering/task_01_jinja.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Basic flask application that renders a HTML template"""
+from flask import Flask, render_template
+from flask import cli
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
