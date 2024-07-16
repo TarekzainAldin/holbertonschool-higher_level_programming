768706b351cfba269a647f4ed08f23f44224d86f
TAREKZAINALDIN <tarekzindin@gmail.com>
2024-07-16 10:13:45 +0200

    finsh the task json and return list in the json file insaid the html

---
+++ python-server_side_rendering/task_02_logic.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Basic flask application that renders a HTML template"""
+from flask import Flask, render_template
+import json
+
+app = Flask(__name__, template_folder="/home/tarek/holbertonschool-higher_level_programming-12/python-server_side_rendering/templates ")
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/items')
+def items():
+    # Read and and load items from json file to python dict
+    with open('/home/tarek/holbertonschool-higher_level_programming-12/python-server_side_rendering/items.json') as f:
+        data = json.load(f)
+    # retrieve values of "items" key
+    # assign empty list if 'items' not present
+    items = data.get('items', [])
+
+    # Put items into html template
+    return render_template('items.html', items=items)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
